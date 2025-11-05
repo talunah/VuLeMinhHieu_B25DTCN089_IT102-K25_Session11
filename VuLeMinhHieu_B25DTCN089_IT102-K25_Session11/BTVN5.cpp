95235f6c90554cc4d46a740dae4ac4a3c3f79bbd
talunah <19yuyuneko@gmail.com>
2025-11-05 22:50:03 +0700

    Update BTVN5.cpp

--- VuLeMinhHieu_B25DTCN089_IT102-K25_Session11/BTVN5.cpp
+++ VuLeMinhHieu_B25DTCN089_IT102-K25_Session11/BTVN5.cpp
@@ -1,23 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
-	int i,searchValue;
-	int isExist=0;
-	int arr[]={6,64,23,5,26,36};
-	int size=sizeof(arr)/sizeof(arr[0]);
-	printf("Gia tri cua ca phan tu trong mang:\n");
-	for(i=0;i<size;i++){
-		printf("a[%d]: %d\n",i,arr[i]);
-	}
-	printf("Nhap vao gia tri can tim:");
-	scanf("%d",&searchValue);
-	for(i=0;i<size;i++){
-		if(arr[i] == searchValue){
-			isExist = 1;
-			printf("Phan tu co chi so %d co gia tri bang %d\n",i,searchValue);
+	int i, j;
+	int originalSize, searchSize;
+	int is_Match_Flag, found_Index = -1;
+printf("nhap so ky tu trong chuoi goc: ");
+scanf("%d", &originalSize);
+char originalArr[originalSize];
+for(i =0; i < originalSize; i++){
+	fflush(stdin);
+	printf("originalArr [%d] = ", i);
+	scanf("%c", &originalArr[i]);
+}	
+printf("nhap so ky tu trong chuoi tim kiem: ");
+scanf("%d", &searchSize);
+char searchArr[searchSize];
+for(i =0; i < searchSize; i++){
+	fflush(stdin);
+	printf("originalArr [%d] = ", i);
+	scanf("%c", &searchArr[i]);
+}
+for(i = 0; i <  originalSize; i++ ){
+	is_Match_Flag = 1;
+	for(j = 0; j < searchSize; j++){
+		if(originalArr[i+j] != searchArr[j]){
+			is_Match_Flag = 0;
 			break;
 		}
 	}
-	if(!isExist){
-		printf("Khong tim thay phan tu nao co gia tri la %d\n",searchValue);
+	if(is_Match_Flag == 1){
+		found_Index = i;
+		break;
+	}	
+}
+printf("\n---KET QUA---\n");
+if(found_Index != -1){
+	printf("tim thay ");
+	for(i = 0; i < searchSize;i++){
+		printf("%c", searchArr[i]);
 	}
+	printf(" chuoi bat dau o chi so %d", found_Index );
+}else{
+	printf("khong tim thay chuoi tuong ung !");
+}
+	
+	
+
+
+	
+	
+	
 }
